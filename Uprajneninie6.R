86c66f83a2ee60dfd7bbb24222eb314cd2b2924d
Code-sto <71377132+Code-sto@users.noreply.github.com>
2020-10-07 12:55:09 +0300

    Update Uprajneninie6.R

--- Uprajneninie6.R
+++ Uprajneninie6.R
@@ -4,11 +4,11 @@ x <- rt(300,30,4)
 e <- rnorm(300,0,13)
 
 layout(matrix(c(1,2),2,1,byrow=TRUE))
-hist(x, freq = F)
+hist(x, freq = F,ylim=c(0,0.4))
 curve(dnorm(x, mean = mean(x), sd = sd(x)),col = "darkblue", lwd = 2, add = T)
 lines(density(x), col = "red", lwd = 3)
 
-hist(e, freq = F)
+hist(e, freq = F,ylim=c(0,0.035))
 curve(dnorm(x, mean = mean(e), sd = sd(e)),col = "darkblue", lwd = 2, add = T)
 lines(density(e), col = "red", lwd = 3)
 dev.off()
